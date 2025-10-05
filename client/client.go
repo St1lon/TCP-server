9665d825255b0978d5d976798a2528ef74bb898f
St1lon <dima.musolin@gmail.com>
2025-10-06 01:15:56 +0300

    ful hw

--- client/client.go
+++ client/client.go
@@ -1,39 +1,38 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
 	"net"
 	"os"
-	"strings"
+)
+
+const (
+	HOST = "localhost"
+	TYPE = "tcp"
+	PORT = "8080"
 )
 
 func main() {
-	// Подключаемся к серверу
-	conn, err := net.Dial("tcp", "localhost:8080")
+	tcpServer, err := net.ResolveTCPAddr(TYPE, HOST+":"+PORT)
 	if err != nil {
-		log.Fatalf("Error connecting to server: %v", err)
-	}
-	defer conn.Close()
-	
-	log.Println("Connected to server successfully")
-	
-	// Читаем ответ от сервера
-	reader := bufio.NewReader(conn)
-	response, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatalf("Error reading response: %v", err)
-	}
-	
-	// Проверяем что получен ответ "OK\n"
-	response = strings.TrimSpace(response)
-	if response == "OK" {
-		fmt.Println("SUCCESS: Received expected response 'OK' from server")
-	} else {
-		fmt.Printf("FAILURE: Expected 'OK' but received '%s'\n", response)
+		log.Println("Unable to resolve addres " + err.Error())
 		os.Exit(1)
 	}
-	
-	log.Println("Client finished successfully")
+
+	conn, err := net.DialTCP(TYPE, nil, tcpServer)
+	if err != nil {
+		log.Printf("Unable to make a connection to the Server: %v\n", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close() // Закрыть соединение при завершении функции
+
+	recievedBuffer := make([]byte, 1024)
+	n, err := conn.Read(recievedBuffer)
+	if err != nil {
+		log.Printf("Could not receive data sent from server: %v", err.Error())
+	}
+	if string(recievedBuffer[:n]) != "OK" {
+		log.Print("The message is not OK")
+	}
+	log.Printf("Recieved Message: %v", string(recievedBuffer))
 }
